5980d012bce8393a265730b9ddf2b081c8666027
Oleg Samoylenko <osamoylenko@gmail.com>
2019-06-06 13:12:05 +0300

    bazelaws added

---
+++ bazelaws/src/Main.java
@@ -0,0 +1,25 @@
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.async.AsyncResponseTransformer;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+
+public class Main {
+    public static void main(String[] args) {
+        S3Client s3Client = S3Client.create();
+
+        String s = s3Client.getObjectAsBytes(
+                GetObjectRequest.builder().bucket("bucket name").key("key").build()
+        ).asUtf8String();
+
+        System.out.println(s);
+    }
+}
